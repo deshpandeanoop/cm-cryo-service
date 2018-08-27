a6dba78704ed8e5075e4b94d9b3c1de1f4272607
deshpandeanoop <anoop.bhardwaja@gmail.com>
2018-08-27 22:38:32 +0530

    modified files

--- src/main/java/com/cm/cryo/idao/ICryoTankDAO.java
+++ src/main/java/com/cm/cryo/idao/ICryoTankDAO.java
@@ -3,8 +3,9 @@ package com.cm.cryo.idao;
 import java.util.List;
 
 import com.cm.cryo.dto.CryoTankDTO;
+import com.cm.cryo.dto.CryoTankDropDownAttributeDTO;
 
 public interface ICryoTankDAO {
-
 	public List<CryoTankDTO> getTanks();
+	public CryoTankDropDownAttributeDTO getDropDownValues();
 }
