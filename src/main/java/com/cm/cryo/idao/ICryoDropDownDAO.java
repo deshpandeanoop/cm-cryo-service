bd3b576acf08a05bcf85f9d3ab4b3c01cf6614c1
deshpandeanoop <anoop.bhardwaja@gmail.com>
2018-08-27 22:35:41 +0530

    Removing unneccesary files

--- src/main/java/com/cm/cryo/idao/ICryoDropDownDAO.java
+++
@@ -1,7 +0,0 @@
-package com.cm.cryo.idao;
-
-import com.cm.cryo.dto.CryoTankDropDownAttributeDTO;
-
-public interface ICryoDropDownDAO {
-	public CryoTankDropDownAttributeDTO getDropDownValues();
-}
