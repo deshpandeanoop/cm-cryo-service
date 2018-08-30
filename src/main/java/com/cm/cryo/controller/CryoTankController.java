242b046fc07a35a51db491d39b9f3bec40907161
deshpandeanoop <anoop.bhardwaja@gmail.com>
2018-08-30 19:53:07 +0530

    changes

--- src/main/java/com/cm/cryo/controller/CryoTankController.java
+++ src/main/java/com/cm/cryo/controller/CryoTankController.java
@@ -1,5 +1,7 @@
 package com.cm.cryo.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,12 @@ import com.cm.cryo.iservice.ICryoTankService;
 public class CryoTankController {
 	@Autowired
 	private ICryoTankService cryoTankService;
+	@Autowired
+	private HttpServletResponse response;
 
 	@RequestMapping("/loadApp")
 	public CryoMasterDTO loadApp() {
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		return cryoTankService.loadCryoMaster();
 	}
 }
