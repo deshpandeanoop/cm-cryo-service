1b98e922a6803fa47bb97f0e03a60b813249d146
deshpandeanoop <anoop.bhardwaja@gmail.com>
2018-09-17 09:03:32 +0530

    minor changes

--- src/main/java/com/cm/cryo/controller/CryoTankController.java
+++ src/main/java/com/cm/cryo/controller/CryoTankController.java
@@ -1,9 +1,13 @@
 package com.cm.cryo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cm.cryo.dto.CryoMasterDTO;
@@ -16,14 +20,27 @@ public class CryoTankController extends CryoMasterBaseController {
 	@Autowired
 	private ICryoTankService cryoTankService;
 
-	@RequestMapping("/loadApp")
+	@CrossOrigin(origins = "*")
+	@GetMapping("/loadApp")
 	public CryoMasterDTO loadApp() {
-		setResponseParameters();
 		return cryoTankService.loadCryoMaster();
 	}
 
-	@RequestMapping(value = "/getTankDetail", method = RequestMethod.POST)
-	public CryoTankDetailResponseDTO getTankDetail(@RequestBody CryoTankDetailRequestDTO tankDetailRequest) {
-		return cryoTankService.getTankDetail(tankDetailRequest);
+//	@CrossOrigin(origins = "*")
+//	@RequestMapping(value = "/getTankDetail", method = RequestMethod.POST,consumes="application/json")
+//	public CryoTankDetailResponseDTO getTankDetail(@RequestBody CryoTankDetailRequestDTO tankDetailRequest) {
+//		System.out.println("Service is been hit");
+//		return cryoTankService.getTankDetail(tankDetailRequest);
+//	}
+	@CrossOrigin(origins="*")
+	@RequestMapping("/getTankDetail")
+	public CryoTankDetailResponseDTO getTankDetail(@RequestParam("tankId") int tankId,@RequestParam("patientLastName") String patName,@RequestParam("hrnNumber") String hrn,@RequestParam("nirc") int nirc)
+	{
+		CryoTankDetailRequestDTO requestDTO = new CryoTankDetailRequestDTO();
+		requestDTO.setTankId(tankId);
+		requestDTO.setPatientLastName(patName);
+		requestDTO.setHrnNumber(hrn);
+		requestDTO.setNirc(nirc);
+		return cryoTankService.getTankDetail(requestDTO);
 	}
 }
